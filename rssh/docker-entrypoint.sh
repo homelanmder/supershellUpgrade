67de6adfde334d6ad905e1a71709346028d4dba6
helloword <53993222+homelanmder@users.noreply.github.com>
2025-06-12 17:10:21 +0800

    Update docker-entrypoint.sh
    修改启动参数

--- rssh/docker-entrypoint.sh
+++ rssh/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 cd /app/bin
 if [ -d "/data/tls" -a -f "/data/tls/tls.cert" -a -f "/data/tls/tls.key" ]       
 then
-    exec /app/wait-for-it.sh -t 0 flask:5000 -- ./server --datadir /data --webserver --external_address :3232 --tls --tlscert /data/tls/tls.cert --tlskey /data/tls/tls.key
+    exec /app/wait-for-it.sh -t 0 flask:5000 -- ./server --datadir /data --enable-client-downloads  --external_address :3232 --tls --tlscert /data/tls/tls.cert --tlskey /data/tls/tls.key
 else
-    exec /app/wait-for-it.sh -t 0 flask:5000 -- ./server --datadir /data --webserver --external_address :3232 --tls
+    exec /app/wait-for-it.sh -t 0 flask:5000 -- ./server --datadir /data --enable-client-downloads  --external_address :3232 --tls
 fi
